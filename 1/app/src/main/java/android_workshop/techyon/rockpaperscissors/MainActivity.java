f4cd9316828bb0d9d846b8e266a31d0eb85b2673
joanna-chen <joannachen.x@gmail.com>
2017-06-29 15:13:42 -0400

    press buttons and get the two hands

--- 1/app/src/main/java/android_workshop/techyon/rockpaperscissors/MainActivity.java
+++ 1/app/src/main/java/android_workshop/techyon/rockpaperscissors/MainActivity.java
@@ -2,9 +2,12 @@ package android_workshop.techyon.rockpaperscissors;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     ImageView computerHand;
@@ -32,8 +35,46 @@ public class MainActivity extends AppCompatActivity {
         rockButton.setText("rock");
         paperButton.setText("paper");
         scissorsButton.setText("scissors");
+
+        rockButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userHand.setImageResource(R.drawable.rock);
+                getComputerHand();
+            }
+        });
+
+        paperButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userHand.setImageResource(R.drawable.paper);
+                getComputerHand();
+            }
+        });
+
+        scissorsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userHand.setImageResource(R.drawable.scissors);
+                getComputerHand();
+            }
+        });
     }
 
-
+    private void getComputerHand() {
+        Random rand = new Random();
+        int n = rand.nextInt(3);
+        switch (n) {
+            case 0:
+                computerHand.setImageResource(R.drawable.rock);
+                break;
+            case 1:
+                computerHand.setImageResource(R.drawable.paper);
+                break;
+            case 2:
+                computerHand.setImageResource(R.drawable.scissors);
+                break;
+        }
+    }
 
 }
