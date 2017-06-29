df13c05c942175bbb208b806c0163ba900912bbb
joanna-chen <joannachen.x@gmail.com>
2017-06-29 15:29:27 -0400

    add score logic

--- 1/app/src/main/java/android_workshop/techyon/rockpaperscissors/MainActivity.java
+++ 1/app/src/main/java/android_workshop/techyon/rockpaperscissors/MainActivity.java
@@ -9,13 +9,23 @@ import android.widget.TextView;
 
 import java.util.Random;
 
+import static android_workshop.techyon.rockpaperscissors.MainActivity.Hand.PAPER;
+import static android_workshop.techyon.rockpaperscissors.MainActivity.Hand.ROCK;
+import static android_workshop.techyon.rockpaperscissors.MainActivity.Hand.SCISSORS;
+
 public class MainActivity extends AppCompatActivity {
+    enum Hand {ROCK, PAPER, SCISSORS};
+
     ImageView computerHand;
     ImageView userHand;
     TextView scoreText;
     Button rockButton;
     Button paperButton;
     Button scissorsButton;
+    Hand computer;
+    Hand user;
+    int computerScore = 0;
+    int userScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +50,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 userHand.setImageResource(R.drawable.rock);
+                user = ROCK;
                 getComputerHand();
+                updateScore();
             }
         });
 
@@ -48,7 +60,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 userHand.setImageResource(R.drawable.paper);
+                user = PAPER;
                 getComputerHand();
+                updateScore();
             }
         });
 
@@ -56,7 +70,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 userHand.setImageResource(R.drawable.scissors);
+                user = SCISSORS;
                 getComputerHand();
+                updateScore();
             }
         });
     }
@@ -67,14 +83,44 @@ public class MainActivity extends AppCompatActivity {
         switch (n) {
             case 0:
                 computerHand.setImageResource(R.drawable.rock);
+                computer = ROCK;
                 break;
             case 1:
                 computerHand.setImageResource(R.drawable.paper);
+                computer = PAPER;
                 break;
             case 2:
                 computerHand.setImageResource(R.drawable.scissors);
+                computer = SCISSORS;
                 break;
         }
+    }
+
+    private void updateScore() {
+        switch (user) {
+            case ROCK:
+                if (computer == PAPER) {
+                    computerScore++;
+                } else if (computer == SCISSORS) {
+                    userScore++;
+                }
+                break;
+            case PAPER:
+                if (computer == SCISSORS) {
+                    computerScore++;
+                } else if (computer == ROCK) {
+                    userScore++;
+                }
+                break;
+            case SCISSORS:
+                if (computer == ROCK) {
+                    computerScore++;
+                } else if (computer == PAPER) {
+                    userScore++;
+                }
+                break;
+        }
+        scoreText.setText("You: " + userScore + " Computer: " + computerScore);
     }
 
 }
